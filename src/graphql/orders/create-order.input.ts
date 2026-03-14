fb5e917bdc11658e41175bc3454d09113df5c34d
liubomyrfedyshyn <LFedyshyn@ecit.com>
2026-03-14 14:09:39 +0200

    fixed comments

--- src/graphql/orders/create-order.input.ts
+++ src/graphql/orders/create-order.input.ts
@@ -1,13 +1,10 @@
-import { Field, ID, InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { ArrayNotEmpty, ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
 import { CreateOrderItemInput } from '../orderItems/create-order-item.input';
 
 @InputType()
 export class CreateOrderInput {
-  @Field(() => ID)
-  // @IsUUID()
-  // userId!: string;
   @Field(() => [CreateOrderItemInput])
   @ArrayNotEmpty()
   @ValidateNested({ each: true })
